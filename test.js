ef064f477f5d75719c131dcde92317f30668ad42
Richard Littauer <richard+github@burntfen.com>
2018-12-04 17:56:23 -0500

    tests: Add test descriptions

--- test.js
+++ test.js
@@ -1,25 +1,26 @@
 import test from 'ava'
 import fn from './'
 
-test(t => {
+test('Gets the right user date for a provided user', t => {
   return fn('RichardLitt').then(result => {
     t.is(result[0].login, 'RichardLitt')
   })
 })
 
-test(t => {
+test('Returns an empty array for an invalid user', t => {
   return fn('wkjhfw9a987sya323jkbsd7822dfsa').then(result => {
     t.deepEqual(result, [])
   })
 })
 
-test(t => {
+test('Returns only valid information for an array with one invalid user', t => {
   return fn(['wkjhfw9a987sya323jkbsd7822dfsa', 'RichardLitt']).then(result => {
-    t.deepEqual(result, [])
+    t.deepEqual(result[0].login, 'RichardLitt')
+    t.deepEqual(result.length, 1)
   })
 })
 
-test(t => {
+test('Returns all data for multiple users', t => {
   return fn(['RichardLitt', 'sindresorhus']).then(result => {
     t.is(result[0].login, 'RichardLitt')
     t.is(result[1].login, 'sindresorhus')
